6454d75387e5e05f74a9f064bd72bf583b3bcedd
Kevin F <Freymaurer@gmx.de>
2026-06-16 15:09:16 +0200

    try publish to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Delight/",
   plugins: [react(), tailwindcss(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
